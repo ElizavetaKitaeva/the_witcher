cda50058b58aaffb14f64f5cfebff2d5d5a6f945
ElizavetaKitaeva <67151257+ElizavetaKitaeva@users.noreply.github.com>
2020-06-30 00:56:39 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+        nextEl: '.arrow',
+    },  
+    breakpoints: {
+        540: {
+            slidesPerView: 2
+        }
+    }
+});
+
+var menubutton = document.querySelector('.menu-button');
+var menu =  document.querySelector('.header');
+menubutton.addEventListener('click', function() {
+    menubutton.classList.toggle('menu-button-active');
+    menu.classList.toggle('header-active');
+});
